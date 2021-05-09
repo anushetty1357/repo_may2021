38d6156dc18c40dab2ac19054e84897858b34398
anusha <anushetty1357@gmail.com>
2021-05-09 04:47:13 +0000

    Core Java2

--- src/test/PrimeNumberCheck.java
+++ src/test/PrimeNumberCheck.java
@@ -7,22 +7,23 @@ public class PrimeNumberCheck {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-Scanner rama = new Scanner(System.in);
-		
-		System.out.println("Enter the number:");
-		
-		int number = rama.nextInt();
-		
-		if(int i=2;i <=num / 2;++i) {
-			
-		}
-	
-	
-	
-	
-	
-	
-	
+Scanner scanner = new Scanner (System.in);
+
+System.out.println("Enter the Number:");
+
+int number = scanner.nextInt();
+
+if (number%number ==1)
+
+{ 
+	System.out.println("Number is prime");
+}
+
+else
+	{ System.out.println("Number is not prime");
+
+}
+
 	
 	}
 
